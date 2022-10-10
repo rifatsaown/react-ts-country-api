c0758f5a5b5ac3cf72992dde4b9933f1728b65e5
rifatsaown <rifatsaown0@gmail.com>
2022-10-11 01:04:52 +0600

    api loaded

--- src/App.tsx
+++ src/App.tsx
@@ -17,28 +17,42 @@ function App() {
       .then((data) => setCountries(data));
   }, []);
   console.log(countries);
-  
+
   return (
     <div className="App">
-      {
-        countries.map((country:icounties) => <ShowCountry name={country.name} altSpellings={country.altSpellings} region={country.region} capital={country.capital} population={country.population}></ShowCountry>)
-      }
+      {countries.map((country: icounties) => (
+        <ShowCountry
+          name={country.name}
+          altSpellings={country.altSpellings}
+          region={country.region}
+          capital={country.capital}
+          population={country.population}
+        ></ShowCountry>
+      ))}
     </div>
   );
 }
 
-const ShowCountry = (props:icounties) => {
+const ShowCountry = (props: icounties) => {
   return (
     <>
-    <div>
-      <h1>Name :{props.name}</h1>
-      <h2>Capital :{props.capital}</h2>
-      <h3>Popilation :{props.population}</h3>
-      <h4>Regioun :{props.region}</h4>
-      <h5>AltSpellings :{props.altSpellings}</h5>
-    </div>
+      <div>
+        <h1>Name :{props.name}</h1>
+        <h2>Capital :{props.capital}</h2>
+        <h3>Popilation :{props.population}</h3>
+        <h4>Regioun :{props.region}</h4>
+        <h5>
+          AltSpellings :
+          {props.altSpellings?.map(
+            // show altSpellings if it is not undefined with a comma between them 
+            (altSpellings) => altSpellings +  
+            // if it is the last element of the array, don't add a comma
+            (props.altSpellings?.indexOf(altSpellings) === Number(props.altSpellings?.length) - 1 ? "" : ", ")
+          )}
+        </h5>
+      </div>
     </>
-  )
+  );
 };
 
 export default App;
