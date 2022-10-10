9db25f0e3e4389a11760c5a8eb42313fda6739ec
rifatsaown <rifatsaown0@gmail.com>
2022-10-11 00:44:47 +0600

    name loaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+interface icounties {
+  name: string;
+  capital: string;
+  population: number;
+  region: string;
+}
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  console.log(countries);
+  
   return (
     <div className="App">
-      <h1>Hello React TypeScript</h1>
+      {
+        countries.map((country:icounties) => <h1>{country.name}</h1>)
+      }
     </div>
   );
 }
+
+const ShowCountry = (props:icounties) => {
+
+};
 
 export default App;
